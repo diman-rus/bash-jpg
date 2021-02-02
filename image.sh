16bbd71f9ac1326fa5b1b6eede1b2a90626f0507
diman-rus <diman.rus@mail.ru>
2021-02-02 23:11:58 +0700

    Test in School 21

---
+++ image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#find ./*.jpg
+if [[ -z $1 ]]
+
+then
+  echo "File selected from current directory."
+  for f in *.jpg
+  do
+    echo $f
+    if [[ $f != *"_thumbnail.jpg"* ]]
+    then
+      convert -quality 90 -resize 320x320 "$f" "./${f%.jpg}_thumbnail.jpg"
+      echo "Create file ${f%.jpg}_thumbnail.jpg"
+    fi
+  done
+else
+  echo "File selected from file $1"
+  while IFS= read -r ff
+  do
+    echo $ff
+    if [[ $ff != *"_thumbnail.jpg"* ]]
+    then
+      convert -quality 90 -resize 320x320 "$ff" "./${ff%.jpg}_thumbnail.jpg"
+      echo "Create file ${ff%.jpg}_thumbnail.jpg"
+    fi
+  done < $1
+fi
